812379cff5da9f41bf1ed16a0fff7db9047fe0e1
ISSOtm <me@eldred.fr>
2025-01-02 18:38:46 +0100

    Try to catch more invalid invocations of `help-gen.sh`

--- manual/help-gen.sh
+++ manual/help-gen.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -eq 0 ]] && ! tty -s; then
 	cd "$(dirname "$(realpath "$0")")"
 	COLUMNS=80 env -C ../teNOR cargo r -q -- -h >src/teNOR.help </dev/null
 	jq '.[1]'
-elif [[ "$1" = "supports" ]]; then
+elif [[ "${1-}" = "supports" ]]; then
 	exit 0 # All renderers are supported.
 else
 	echo 'This script is only meant to be executed as a mdBook preprocessor.' >&2
